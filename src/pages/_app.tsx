b0788a6c9edfc98554b05261ae26ea8a12bd26dd
Sidnei Farias <sidneijr.77@gmail.com>
2023-09-07 18:46:13 -0300

    Implementa os estilos globais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
 import type { AppProps } from 'next/app'
 
+import { globalStyles } from '@/styles/global'
+
+// Deve estar no escopo global, para que tais estilos não sejam carregados novamente em cada página.
+globalStyles()
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
