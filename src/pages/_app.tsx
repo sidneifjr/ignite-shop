ed888c14b493cbb78e12d35fb82ffedd29b832aa
Sidnei Farias <sidneijr.77@gmail.com>
2023-09-07 19:00:07 -0300

    Configura o Header da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import type { AppProps } from 'next/app'
 
 import { globalStyles } from '@/styles/global'
+import { Container, Header } from '@/styles/pages/app'
+
+import logoImg from '../assets/ignite-shop-logo.svg'
 
 // Deve estar no escopo global, para que tais estilos não sejam carregados novamente em cada página.
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="logo" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
---
+++ src/styles/pages/app.ts
@@ -0,0 +1,17 @@
+import { styled } from "..";
+
+export const Container = styled('div', {
+  minHeight: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+})
+
+export const Header = styled('header', {
+  width: '100%',
+  maxWidth: 1180,
+  padding: '2rem 0',
+  marginRight: 'auto',
+  marginLeft: 'auto'
+})
