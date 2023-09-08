7bab39fd9376bc20a5e2e3e8e6737450722bfd12
Sidnei Farias <sidneijr.77@gmail.com>
2023-09-08 18:50:58 -0300

    Inclui a estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,40 @@
-import { styled } from "@/styles"
+import Image from "next/image"
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
+import { HomeContainer, Product } from "@/styles/pages/home"
 
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+import camiseta1 from '../assets/carousel/1.png'
+import camiseta2 from '../assets/carousel/2.png'
+import camiseta3 from '../assets/carousel/3.png'
 
 export default function Home() {
   return (
-    <>
-      <h1>Hello World</h1>
-      <Button>
-        <span>Teste</span>
-        Enviar</Button>
-    </>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta3} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta Z</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
