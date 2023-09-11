bb4ecb57afa42e3852e70778624040481dd7b0cf
Sidnei Farias <sidneijr.77@gmail.com>
2023-09-11 19:04:40 -0300

    Busca produtos cadastrados no Stripe e os manipula na Home

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // Necessário para que o Next possa realizar o processo de otimização dos assets provenientes de um domínio específico.
+  images: {
+    domains: [
+      'files.stripe.com'
+    ]
+  }
 }
 
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react"
 import Image from "next/image"
+import { stripe } from "@/lib/stripe"
+import { GetServerSideProps } from "next"
 
 import { useKeenSlider } from 'keen-slider/react'
 
@@ -9,8 +12,20 @@ import camiseta2 from '../assets/carousel/2.png'
 import camiseta3 from '../assets/carousel/3.png'
 
 import 'keen-slider/keen-slider.min.css'
+import Stripe from "stripe"
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
+  const [list, setList] = useState<number[]>([])
+
   const [ sliderRef ] = useKeenSlider({
     slides: {
       spacing: 48,
@@ -18,43 +33,55 @@ export default function Home() {
     }
   })
 
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('rodou')
+
+      setList([1,2,3])
+    }, 2000)
+  }, [])
+
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
-      <Product className="keen-slider__slide">
-        <Image src={camiseta1} alt="" width={520} height={480} />
+      {products.map(product => {
+        return (
+          <Product key={product.id} className="keen-slider__slide">
+            <Image src={product.imageUrl} alt="" width={520} height={480} />
 
-        <footer>
-          <strong>Camiseta X</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={camiseta2} alt="" width={520} height={480} />
-
-        <footer>
-          <strong>Camiseta Y</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={camiseta3} alt="" width={520} height={480} />
-
-        <footer>
-          <strong>Camiseta Z</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
-
-      <Product className="keen-slider__slide">
-        <Image src={camiseta3} alt="" width={520} height={480} />
-
-        <footer>
-          <strong>Camiseta W</strong>
-          <span>R$ 79,90</span>
-        </footer>
-      </Product>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        )
+      })}
+      {/* <pre>{JSON.stringify(products)}</pre> */}
     </HomeContainer>
   )
+}
+
+// Roda no servidor Node, provisionado pelo Next; útil quando queremos realizar uma chamada de API oculta ao usuário final.
+// Posso inserir código de autenticação, acesso a banco de dados, requisições, dados sensíveis, etc.
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price']
+  })
+  console.log(response.data)
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount / 100,
+    }
+  })
+
+  return {
+    props: {
+      products
+    }
+  }
 }
