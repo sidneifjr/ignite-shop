b9eb9c1d77f4b3892627d78e4a663ca38504f921
Sidnei Farias <sidneijr.77@gmail.com>
2023-09-11 23:11:42 -0300

    Habilita SSG na Home e o revalidate correspondente

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,8 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY as string
+
+export const stripe = new Stripe(stripeSecretKey, {
   apiVersion: '2023-08-16',
   appInfo: {
     name: 'Ignite Shop'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,10 @@
-import { useEffect, useState } from "react"
 import Image from "next/image"
 import { stripe } from "@/lib/stripe"
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 
 import { useKeenSlider } from 'keen-slider/react'
 
 import { HomeContainer, Product } from "@/styles/pages/home"
-
-import camiseta1 from '../assets/carousel/1.png'
-import camiseta2 from '../assets/carousel/2.png'
-import camiseta3 from '../assets/carousel/3.png'
 
 import 'keen-slider/keen-slider.min.css'
 import Stripe from "stripe"
@@ -24,22 +19,12 @@ interface HomeProps {
 }
 
 export default function Home({ products }: HomeProps) {
-  const [list, setList] = useState<number[]>([])
-
   const [ sliderRef ] = useKeenSlider({
     slides: {
       spacing: 48,
       perView: 3,
     }
   })
-
-  useEffect(() => {
-    setTimeout(() => {
-      console.log('rodou')
-
-      setList([1,2,3])
-    }, 2000)
-  }, [])
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
@@ -60,28 +45,31 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
+// Posso usar duas opções: getServerSideProps (habilita SSR) e getStaticProps (habilita SSG).
 // Roda no servidor Node, provisionado pelo Next; útil quando queremos realizar uma chamada de API oculta ao usuário final.
 // Posso inserir código de autenticação, acesso a banco de dados, requisições, dados sensíveis, etc.
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   })
-  console.log(response.data)
 
   const products = response.data.map(product => {
     const price = product.default_price as Stripe.Price
+    const priceUnit = price.unit_amount as number
 
     return {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount / 100,
+      price: priceUnit / 100,
     }
   })
 
   return {
     props: {
       products
-    }
+    },
+
+    revalidate: 60 * 60 * 2 // 2 hours
   }
 }
