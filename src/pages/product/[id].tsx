eaeb79333aad90aab85ee24787fa0d3bf1cfb8cf
Sidnei Farias <sidneijr.77@gmail.com>
2023-09-13 19:12:21 -0300

    Inclui o getStaticPaths em Product

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,6 @@
 import { stripe } from "@/lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
-import { GetStaticProps } from "next"
+import { GetStaticPaths, GetStaticProps } from "next"
 import Image from "next/image";
 
 import Stripe from "stripe";
@@ -32,6 +32,15 @@ export default function Product({ product }: ProductProps) {
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async() => {
+  return {
+    paths: [
+      { params: {id: 'prod_ObQs7vu22lgsww'} },
+    ],
+    fallback: false
+  }
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
