5e9fae89ab80c9124eda71c84b30b30b48a44def
Sidnei Farias <sidneijr.77@gmail.com>
2023-09-12 21:48:04 -0300

    Cria a tela do Produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,23 @@
-import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
+// import { useRouter } from "next/router"
 
 export default function Product() {
-  const { query } = useRouter()
+  // const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+        
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis delectus omnis officiis debitis quasi rerum aspernatur exercitationem, molestias dolore, dicta id ex deserunt magni nulla, sapiente suscipit officia vel? Recusandae.</p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
