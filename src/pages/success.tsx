881018ae7791bc4a8c11ffd1b2c8c9484a6f5b87
Sidnei Farias Jr. <sfarias.dev@gmail.com>
2023-09-20 00:59:03 -0300

    Monta tela de 'Success'

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,20 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+        
+      </ImageContainer>
+
+      <p>Uhuul <strong>Diego Fernandes</strong>, sua <strong>Camiseta Beyond the Limits</strong> ja esta a caminho da sua casa.</p>
+      
+      <Link href="/">
+        Voltar ao catalogo
+      </Link>
+    </SuccessContainer>
   )
 }
