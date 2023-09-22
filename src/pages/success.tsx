15f4f4bed3c76f93cb9d1a49afd531c569688406
Sidnei Farias Jr. <sfarias.dev@gmail.com>
2023-09-21 23:16:34 -0300

    Configura integração entre o ESLint e Prettier

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,61 +1,70 @@
-import { stripe } from "@/lib/stripe";
-import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
-import { GetServerSideProps } from "next";
-import Link from "next/link";
-import Stripe from "stripe";
+import { stripe } from '@/lib/stripe';
+import { ImageContainer, SuccessContainer } from '@/styles/pages/success';
+import { GetServerSideProps } from 'next';
+import Link from 'next/link';
+import Stripe from 'stripe';
 
-import Image from "next/image";
-import Head from "next/head";
+import Image from 'next/image';
+import Head from 'next/head';
 
 interface SuccessProps {
-  customerName: string
+  customerName: string;
   product: {
-    name: string
-    imageUrl: string
-  }
+    name: string;
+    imageUrl: string;
+  };
 }
 
-export default function Success({customerName, product}: SuccessProps) {
+export default function Success({ customerName, product }: SuccessProps) {
   return (
     <>
       <Head>
         <title> Compra efetuada | Ignite Shop</title>
-        <meta name="robots" content="noindex" />{/* Evita que os crawlers indexem essa tela. */} 
+        <meta
+          name="robots"
+          content="noindex"
+        />
+        {/* Evita que os crawlers indexem essa tela. */}
       </Head>
 
       <SuccessContainer>
         <h1>Compra efetuada!</h1>
 
         <ImageContainer>
-          <Image src={product.imageUrl} alt="" width={120} height={110}  />
+          <Image
+            src={product.imageUrl}
+            alt=""
+            width={120}
+            height={110}
+          />
         </ImageContainer>
 
-        <p>Uhuul <strong>{customerName}</strong>, sua <strong>{product.name}</strong> ja esta a caminho da sua casa.</p>
-        
-        <Link href="/">
-          Voltar ao catalogo
-        </Link>
+        <p>
+          Uhuul <strong>{customerName}</strong>, sua{' '}
+          <strong>{product.name}</strong> ja esta a caminho da sua casa.
+        </p>
+
+        <Link href="/">Voltar ao catalogo</Link>
       </SuccessContainer>
     </>
-  )
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-  if(!query.session_id){
+  if (!query.session_id) {
     return {
       redirect: {
         destination: '/',
         permanent: false, // o redirect nao e permamente, ocorrendo apenas quando nao houver o session_id.
-
-      }
-    }
+      },
+    };
   }
-  
-  const sessionId = String(query.session_id)
+
+  const sessionId = String(query.session_id);
 
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
-    expand: ['line_items', 'line_items.data.price.product']
-  })
+    expand: ['line_items', 'line_items.data.price.product'],
+  });
 
   const customerName = session.customer_details.name;
   const product = session.line_items.data[0].price.product as Stripe.Product;
@@ -65,8 +74,8 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
       customerName,
       product: {
         name: product.name,
-        imageUrl: product.images[0]
-      }
-    }
-  }
-}
+        imageUrl: product.images[0],
+      },
+    },
+  };
+};
