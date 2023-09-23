89bb666709761d9c875edf9f82db667a17dfb000
Sidnei Farias Jr. <sfarias.dev@gmail.com>
2023-09-22 21:06:49 -0300

    Remove estilos associados ao Header, em app.ts

--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -7,23 +7,3 @@ export const Container = styled('div', {
   flexDirection: 'column',
   alignItems: 'flex-start',
 });
-
-export const Header = styled('header', {
-  width: '100%',
-  maxWidth: 1440,
-  padding: '2rem 0',
-  marginRight: 'auto',
-  marginLeft: 'auto',
-  display: 'flex',
-  justifyContent: 'space-between',
-
-  button: {
-    width: '3rem',
-    height: '3rem',
-    border: 'none',
-    borderRadius: '0.375rem',
-    padding: '0.75rem',
-    background: '$gray800',
-    cursor: 'pointer',
-  },
-});
